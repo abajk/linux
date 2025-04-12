9df9e321595d9488a0bb0067b5e1b24101dd2a84
Aleksander Jan Bajkowski <olek2@wp.pl>
2025-04-12 22:13:31 +0200

    net: ethernet: intel: igc: remove redundant device name setup
    Signed-off-by: Aleksander Jan Bajkowski <olek2@wp.pl>

--- drivers/net/ethernet/intel/igc/igc_main.c
+++ drivers/net/ethernet/intel/igc/igc_main.c
@@ -7198,7 +7198,6 @@ static int igc_probe(struct pci_dev *pdev,
 	 */
 	igc_get_hw_control(adapter);
 
-	strscpy(netdev->name, "eth%d", sizeof(netdev->name));
 	err = register_netdev(netdev);
 	if (err)
 		goto err_register;
